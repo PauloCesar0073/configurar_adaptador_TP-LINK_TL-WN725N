8981bdd6ec0b666168e0d41d64ab0eade3008e0e
Paulo Cesar <129324033+PauloCesar0073@users.noreply.github.com>
2023-07-11 02:19:43 -0300

    Update install.py

--- install.py
+++ install.py
@@ -1,37 +1,31 @@
-import subprocess
+import os
 import configparser
 
 
-
-
-def run_command(command):
-    subprocess.run(command, shell=True)
-
-
-def dependecias():
-
-    run_command(['pip install subprocess'])
-    run_command(['pip install configparser'])
-
+def dependencies():
+    os.system('pip install configparser')
 
 
 def update_system():
     print('Passo 1: Atualizar o Sistema\n')
-    run_command(['sudo apt update'])
-    run_command(['sudo apt upgrade'])
+    os.system('sudo apt update')
+    os.system('sudo apt upgrade')
+
 
 def install_driver():
     print('Passo 2: Instalar o Driver\n')
-    run_command(['sudo apt install realtek-rtl8188eus*'])
+    os.system('sudo apt install realtek-rtl8188eus*')
+
 
 def add_to_blacklist():
     print('Passo 3: Adicionar o Adaptador à Blacklist\n')
-    run_command("echo 'blacklist r8188eus' | sudo tee -a '/etc/modprobe.d/realtek.conf'")
+    os.system("echo 'blacklist r8188eus' | sudo tee -a '/etc/modprobe.d/realtek.conf'")
+
 
 def configure_network_manager():
     print('Passo 4: Configurar o NetworkManager\n')
     print('Passo 4.1. Obter o Endereço MAC do Adaptador Wi-Fi\n')
-    mac_address = subprocess.run(["ifconfig wlan0 | awk '/ether/ {print $2} /unspec/ {print $2}'"], capture_output=True, text=True).stdout.strip()
+    mac_address = os.popen("ifconfig wlan0 | awk '/ether/ {print $2} /unspec/ {print $2}'").read().strip()
 
     print('Passo 4.2. Editar o Arquivo NetworkManager.conf\n')
     config = configparser.ConfigParser()
@@ -39,11 +33,12 @@ def configure_network_manager():
 
     if 'keyfile' not in config.sections():
         config['keyfile'] = {}
-    
+
     config['keyfile']['unmanaged-devices'] = f'mac:{mac_address}'
 
     with open('/etc/NetworkManager/NetworkManager.conf', 'w') as configfile:
         config.write(configfile)
+
 
 def enable_monitor_mode():
     print("Passo 5: Colocar o Adaptador em Modo Monitor:\n")
@@ -51,26 +46,28 @@ def enable_monitor_mode():
 
     if mode_choice == "1":
         print('Usando airmon-ng\n')
-        run_command(["sudo airmon-ng check kill"])
-        run_command(['sudo airmon-ng start wlan0'])
+        os.system('sudo airmon-ng check kill')
+        os.system('sudo airmon-ng start wlan0')
 
     elif mode_choice == "2":
         print('Usando iwconfig\n')
-        run_command(['sudo ip link set wlan0 down'])
-        run_command(['sudo iw dev wlan0 set type monitor'])
+        os.system('sudo ip link set wlan0 down')
+        os.system('sudo iw dev wlan0 set type monitor')
 
     else:
         print('Opção inválida! Ative manualmente.')
 
+
 def main():
-    dependecias()
+    dependencies()
     update_system()
     install_driver()
     add_to_blacklist()
     configure_network_manager()
     enable_monitor_mode()
 
-    print("Agora é só realizar um scan. Caso não consiga, reinicie o Kali e coloque a interface em modo monitor e tente novamente um scan")
+    print("Agora é só realizar um scan. Caso não consiga, reinicie o Kali, coloque a interface em modo monitor e tente novamente um scan")
+
 
 if __name__ == "__main__":
     main()
